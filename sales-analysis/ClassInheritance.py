50e923d45f976ac09e4693ef282ce45454971141
username <filipioan_29@yahoo.ro>
2026-09-20 09:03:18 +0300

    Updated

--- sales-analysis/ClassInheritance.py
+++ sales-analysis/ClassInheritance.py
@@ -3,7 +3,7 @@ class Animal:
     def __init__(self, name):
         self.name = name
     
-    def eat(self):
+    def eat (self):
         return f"{self.name} is eating"
     
     def sleep(self):
